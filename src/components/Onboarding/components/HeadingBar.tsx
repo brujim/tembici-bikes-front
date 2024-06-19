b7fea9181f5d146df05d7dcb0540ff2bd0dbf152
Maycon Oliveira <rick.maycon.98@gmail.com>
2024-06-19 15:01:27 -0300

    feat: jump button

--- src/components/Onboarding/components/HeadingBar.tsx
+++ src/components/Onboarding/components/HeadingBar.tsx
@@ -26,7 +26,12 @@ export const HeadingBar = ({
   }
 
   return (
-    <div className="w-[100vw] bg-pearl h-[90px] text-[16px] font-semibold text-cooper flex justify-between items-center px-5">
+    <div 
+      style={{
+        justifyContent: activeStep === 1 ? 'end' : ''
+      }}
+      className={`w-[100vw] bg-pearl h-[90px] text-[16px] font-semibold text-cooper flex justify-between items-center px-5 `}>
+      {activeStep !== 1 && <p onClick={previousButton}>Voltar</p>}
       <p
         onClick={() => {
           navigator.geolocation.getCurrentPosition(onSuccess, onFailure)
@@ -34,7 +39,6 @@ export const HeadingBar = ({
       >
         Pular
       </p>
-      {activeStep !== 1 && <p onClick={previousButton}>Voltar</p>}
     </div>
   )
 }
