e05acd4e4f5b2b154304657d79ab0a1853cd47aa
Maycon Oliveira <rick.maycon.98@gmail.com>
2024-07-01 14:53:40 -0300

    filters padding

--- src/components/FilterModal/index.tsx
+++ src/components/FilterModal/index.tsx
@@ -151,8 +151,8 @@ export const FilterModal = ({
             </div>
           </div>
         )}
-        <div className="bg-pearl pt-[44px] h-[100vh] absolute bottom-0 w-[100vw] overflow-y-scroll">
-          <div className="flex justify-between items-center pt-5 px-4">
+        <div className="bg-pearl pt-[48px] h-[100vh] absolute bottom-0 w-[100vw] overflow-y-scroll">
+          <div className="flex justify-between items-center pt-6 px-4">
             <img src="/images/filter/itau.png" />
             <img src="/images/filter/x.png" onClick={closeFilter} />
           </div>
